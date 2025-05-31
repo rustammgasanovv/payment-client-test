3ed1b1f4a95af335d21b4695385769390e1e64de
Rustam Gasanov <rustammgasanovv@gmail.com>
2025-05-31 14:47:10 +0400

    main

---
+++ src/app/result/page.tsx
@@ -0,0 +1,46 @@
+// pages/result.tsx
+
+'use client';
+
+import { useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import axios from 'axios';
+
+export default function ResultPage() {
+  const [status, setStatus] = useState<'loading' | 'success' | 'failed' | 'not_found'>('loading');
+  const [orderId, setOrderId] = useState<string | null>(null);
+  const params = useSearchParams();
+
+  useEffect(() => {
+    const ordId = params.get('orderId');
+    if (!ordId) {
+      setStatus('not_found');
+      return;
+    }
+
+    setOrderId(ordId);
+
+    axios
+      .get(`https://payment-server-test-production.up.railway.app/payment/status?orderId=${ordId}`)
+      .then((res) => {
+        const stat = res.data.status;
+        if (stat === 'success') setStatus('success');
+        else if (stat === 'failed') setStatus('failed');
+        else setStatus('not_found');
+      })
+      .catch(() => setStatus('not_found'));
+  }, [params]);
+
+  if (status === 'loading') return <p className="text-center p-10">Загрузка статуса...</p>;
+
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold mb-4">Результат оплаты</h1>
+      <p className="mb-2">Номер заказа: <strong>{orderId}</strong></p>
+
+      {status === 'success' && <p className="text-green-600">✅ Оплата прошла успешно!</p>}
+      {status === 'failed' && <p className="text-red-600">❌ Оплата не удалась.</p>}
+      {status === 'not_found' && <p className="text-gray-500">Информация о платеже не найдена.</p>}
+    </div>
+  );
+}
