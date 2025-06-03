419edbda67ab61cc0bcc85693f150068238e4ea0
rustammgasanovv <rustammgasanovv@gmail.com>
2025-06-03 17:57:58 +0400

    Update page.tsx

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,13 +1,29 @@
-'use client'
-
-import React from 'react'
+import { useSearchParams } from 'next/navigation';
 
 export default function Success() {
-    return (
-      <div className="flex flex-col justify-center items-center h-screen bg-green-50">
-        <h1 className="text-4xl font-bold text-green-700">Оплата прошла успешно!</h1>
-        <p className="mt-4 text-gray-700">Спасибо за покупку.</p>
-      </div>
-    );
-  }
+  const params = useSearchParams();
+  const orderId = params.get('orderId');
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen bg-green-50">
+      <h1 className="text-4xl font-bold text-green-700">Оплата прошла успешно!</h1>
+      <p className="mt-4 text-gray-700">Спасибо за покупку.</p>
+      {orderId && <p className="mt-2 text-gray-600">Номер заказа: <strong>{orderId}</strong></p>}
+    </div>
+  );
+}
+
+
+// 'use client'
+
+// import React from 'react'
+
+// export default function Success() {
+//     return (
+//       <div className="flex flex-col justify-center items-center h-screen bg-green-50">
+//         <h1 className="text-4xl font-bold text-green-700">Оплата прошла успешно!</h1>
+//         <p className="mt-4 text-gray-700">Спасибо за покупку.</p>
+//       </div>
+//     );
+//   }
   
